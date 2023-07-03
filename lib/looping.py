f2dee552e6a84b13418b93593c4f1ea661992603
Eric Krukowski <erickruk@amazon.com>
2023-07-03 02:09:54 -0400

    Lab complete

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,22 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    for n in range(10):
+        print(n+1)
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_list = list()
+    for i in int_list:
+        squared_list.append(i*i)
+    return squared_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(100):
+        n=i+1
+        if(n%3==0):
+            print("FizzBuzz") if n%5==0 else print("Fizz")
+        else: 
+            print("Buzz") if n%5==0 else print(n)
